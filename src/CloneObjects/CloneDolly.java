83b4489fc9f7f1d97fb1f65a122fd1d2da3900d0
Андрей <andrews.ru83@gmail.com>
2022-07-08 20:11:49 +0300

    Коммит2

---
+++ src/CloneObjects/CloneDolly.java
@@ -0,0 +1,18 @@
+package CloneObjects;
+
+public class CloneDolly implements Cloneable {
+    private String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import CloneObjects.CloneDolly;
 import Interface.Cactus;
 import Interface.Rose;
 import Interface.Test;
@@ -13,6 +14,24 @@ public class Main {
         test.foo(rose);
         test.foo(cactus);
 
+        CloneDolly cloneDolly = new CloneDolly();
+        CloneDolly cloneDolly1 = foo(cloneDolly);
+
+        cloneDolly.setName("Dolly");
+        cloneDolly1.setName("Sheet");
+
+        System.out.println(cloneDolly.getName());
+        System.out.println(cloneDolly1.getName());
+    }
+
+    public static CloneDolly foo(CloneDolly cloneDolly) {
+        CloneDolly dolly = null;
+        try {
+            dolly = (CloneDolly) cloneDolly.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+        return dolly;
     }
 
 }
